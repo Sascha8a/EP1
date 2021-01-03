dfd605af58a6dd67c3b83b4dfbfa2fdf8b4d25db
Alexander Lampalzer <sascha@localhost.systems>
2021-01-03 21:32:49 +0100

    applyFilter

--- Aufgabenblatt5/src/Aufgabe2.java
+++ Aufgabenblatt5/src/Aufgabe2.java
@@ -1,23 +1,51 @@
+import java.util.Arrays;
+
 /*
     Aufgabe 2) Zweidimensionale Arrays - Sortieren und Filtern
 */
-public class  Aufgabe2 {
-    
+public class Aufgabe2 {
+
     private static double[][] genMeanFilter(int n) {
-        // TODO: Implementieren Sie hier Ihre Lösung für die Methode
-        return null; //Zeile kann geändert oder entfernt werden.
+        if (n % 2 == 0 || n < 0) return null;
+
+        double mean = 1.0 / (n * n);
+        double[][] result = new double[n][n];
+
+        for (double[] row : result) Arrays.fill(row, mean);
+
+        return result;
     }
 
     private static double[][] applyFilter(double[][] workArray, double[][] filterArray) {
-        // TODO: Implementieren Sie hier Ihre Lösung für die Methode
-        return null; //Zeile kann geändert oder entfernt werden.
+        int ww = workArray[0].length;       // workArray width
+        int wh = workArray.length;          // workArray height
+        int fw = filterArray[0].length;     // filterArray width
+        int fh = filterArray.length;        // filterArray height
+
+        double[][] result = new double[wh][ww];
+
+        for (int wr = fh / 2; wr < wh - fh / 2; wr++) {         // workArray row
+            for (int wc = fw / 2; wc < ww - fw / 2; wc++) {     // workArray column
+                double mean = 0;
+
+                for (int fr = 0; fr < fh; fr++) {               // filterArray row
+                    for (int fc = 0; fc < fw; fc++) {           // filterArray column
+                        mean += workArray[wr - fh / 2 + fr][wc - fw / 2 + fc] * filterArray[fr][fc];
+                    }
+                }
+
+                result[wr][wc] = mean;
+            }
+        }
+
+        return result;
     }
-    
+
     private static void print(double[][] workArray) {
-        if(workArray != null) {
+        if (workArray != null) {
             for (int y = 0; y < workArray.length; y++) {
                 for (int x = 0; x < workArray[y].length; x++) {
-                    System.out.printf("%.2f",workArray[y][x]);
+                    System.out.printf("%.2f", workArray[y][x]);
                     System.out.print("\t");
                 }
                 System.out.println();
@@ -25,7 +53,7 @@ public class  Aufgabe2 {
             System.out.println();
         }
     }
-    
+
     public static void main(String[] args) {
         double[][] myResultArray;
 
@@ -34,7 +62,7 @@ public class  Aufgabe2 {
         double[][] myFilter2 = genMeanFilter(5);
         print(myFilter2);
 
-        double[][] myArray1 = {{0,0,0,0,0}, {0,1,1,1,0}, {0,1,1,1,0}, {0,1,1,1,0}, {0,0,0,0,0}};
+        double[][] myArray1 = {{0, 0, 0, 0, 0}, {0, 1, 1, 1, 0}, {0, 1, 1, 1, 0}, {0, 1, 1, 1, 0}, {0, 0, 0, 0, 0}};
         print(myArray1);
 
         myResultArray = applyFilter(myArray1, myFilter1);
@@ -42,11 +70,13 @@ public class  Aufgabe2 {
         myResultArray = applyFilter(myArray1, myFilter2);
         print(myResultArray);
 
-        double[][] myArray2 = {{0,0,0,0,0}, {0,0,0,0,0}, {0,1,1,1,0}, {0,0,0,0,0}, {0,0,0,0,0}};
+        double[][] myArray2 = {{0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, {0, 1, 1, 1, 0}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}};
         print(myArray2);
-        //TODO: Erstellen Sie einen Shiftfilter, wenden Sie ihn auf myArray2 an und geben Sie das Ergebnis mittels print() aus
 
+        double[][] myFilter3 = {{0, 1, 0}, {0, 0, 0}, {0, 0, 0}};
+        print(myFilter3);
+
+        myResultArray = applyFilter(myArray2, myFilter3);
+        print(myResultArray);
     }
-    
-    
 }
